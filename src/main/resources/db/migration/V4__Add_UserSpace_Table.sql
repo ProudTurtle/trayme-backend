d8a6535d22b271a2da79837f72db2e048f7190f5
ProudTurtle <a.wierzbicka0579@gmail.com>
2024-11-22 12:36:28 +0100

    Sharowanie space

--- src/main/resources/db/migration/V4__Add_UserSpace_Table.sql
+++ src/main/resources/db/migration/V4__Add_UserSpace_Table.sql
@@ -1,10 +1,10 @@
-CREATE TABLE user_space (
+CREATE TABLE IF NOT EXISTS user_space (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
     space_id INT NOT NULL,
     role VARCHAR(255) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user(id),
-    FOREIGN KEY (space_id) REFERENCES space(id)
+    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE,
+    FOREIGN KEY (space_id) REFERENCES space(id) ON DELETE CASCADE
 );
 
 ALTER TABLE space ADD COLUMN share_key VARCHAR(255) NOT NULL;
