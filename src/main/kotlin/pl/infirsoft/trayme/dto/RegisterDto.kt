c9f1321f91dd4d143572ffcb2352d3816e2693e4
ProudTurtle <a.wierzbicka0579@gmail.com>
2025-01-20 17:43:52 +0100

    ZMIANY: * hotfix registerDTO

--- src/main/kotlin/pl/infirsoft/trayme/dto/RegisterDto.kt
+++ src/main/kotlin/pl/infirsoft/trayme/dto/RegisterDto.kt
@@ -2,7 +2,7 @@ package pl.infirsoft.trayme.dto
 
 data class RegisterDto(
     val token: String,
-    val name: String,
+    val name: String?,
     val email: String?,
     val avatarUrl: String?,
     val spaces: List<SpaceDto>
